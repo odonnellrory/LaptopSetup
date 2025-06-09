7c199e69dd34c22996dc8205edc8020508ca6843
odonnellrory <roryod.134@gmail.com>
2025-06-09 23:42:06 +0100

    oops

--- C:\Temp/install-choco.ps1
+++
@@ -1,5 +0,0 @@
-
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-  
--- C:\Temp/setup.ps1
+++
@@ -1,7 +0,0 @@
-Start-Transcript -Path "C:\Temp/choco-debug.log"
-$env:Path += ";$env:ChocolateyInstall\bin"
-choco install vscode -y --force
-choco install notepadplusplus -y --force
-choco install adobereader -y --force
-choco list --local-only
-Stop-Transcript
